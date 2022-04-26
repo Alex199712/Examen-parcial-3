f7226f25e24c1a5db5c70e9dd5f670e3e432ebbc
Alexander <denilson.mendoza@unah.hn>
2022-04-26 00:05:23 -0600

    EXAMEN FINAL
    este es el código del examen del tercer parcial de LDP 3

---
+++ PROYECTO FINAL/Modelos/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos;
+
+public class Producto
+{
+    [Required(ErrorMessage = "Codigo Obligatorio ")]
+    public string Codigo { get; set; }
+    [Required(ErrorMessage = "Codigo Obligatorio")]
+    public string Descripcion { get; set; }
+    public decimal Precio { get; set; }
+    public int Existencia { get; set; }
+    public byte[] Imagen { get; set; }
+}
---
+++ PROYECTO FINAL/Modelos/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos;
+
+public class Usuario
+{
+    [Required(ErrorMessage = "Codigo Obligatorio")]
+    public string Codigo { get; set; }
+    [Required(ErrorMessage = "Codigo Obligatorio")]
+    public string Nombre { get; set; }
+    [Required(ErrorMessage = "Codigo Obligatorio")]
+    public string Rol { get; set; }
+    public string Clave { get; set; }
+    public bool EstaActivo { get; set; }
+}
